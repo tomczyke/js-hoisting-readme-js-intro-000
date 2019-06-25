0522fc76498066c3107feefe1654afbbac48a3aa
Evan Tomczyk <tomczykej@gmail.com>
2019-06-25 09:12:08 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,18 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
+  // fix the code in here
   var thisIsCrazy = function (){
     console.log("hey!!!")
+  thisIsCrazy();
   }
 }
 
